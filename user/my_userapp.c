0755d82933db585825571880f7af7467ae2e0e19
2022058740 <ykh205@hanyang.ac.kr>
2025-03-24 07:47:55 +0000

    3rd

--- user/my_userapp.c
+++ user/my_userapp.c
@@ -7,6 +7,6 @@ int main(int argc, char *argv[])
 	char *buf = "Hello xv6!";
 	int ret_val;
 	ret_val = myfunction(buf);
-	fprintf(1, “Return value : 0x%x\n”, ret_val);
+	fprintf(1, "Return value : 0x%x\n", ret_val);
 	exit(0);
 };
